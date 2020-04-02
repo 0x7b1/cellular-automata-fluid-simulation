afd0d571b1777ba4fc0b0604296ef90a9b063fee
JCR <vonzo55@gmail.com>
2020-04-02 15:14:12 +0300

    Stable simulation steps, need boundary checks

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ use minifb::{
 };
 use vek::Vec2;
 
-const WIDTH: usize = 100;
-const HEIGHT: usize = 100;
+const WIDTH: usize = 200;
+const HEIGHT: usize = 200;
 const N_NEIGHBORS: u8 = 8;
 const DELTA: f32 = 1.0;
 
@@ -89,19 +89,12 @@ struct Cell {
     conductivity: f32,
 
     element: CellElement,
-
-    // mass: f32,
+    mass: f32,
     // new_mass: f32,
 
     // living: bool,
     // solid: bool,
 }
-
-struct CellWater {}
-
-struct CellAir {}
-
-struct CellGround {}
 
 impl Cell {
     fn empty() -> Self {
@@ -110,29 +103,9 @@ impl Cell {
             population: 0.0,
             spread: 0.0,
             conductivity: 1.0,
+            mass: 0.0,
 
-            element: CellElement::Empty,
-        }
-    }
-
-    fn set_element(&mut self, element: CellElement) {
-        match element {
-            CellElement::Empty => {
-                self.velocity = Vec2::zero();
-                self.population = 0.0;
-            }
-            CellElement::Wall => {
-                self.velocity = Vec2::zero();
-                self.population = 0.0;
-                self.conductivity = 0.0;
-                self.population = 400.0;
-            }
-            CellElement::Water => {
-                self.velocity = Vec2::new(0.0, -0.9);
-                self.spread = 0.5;
-                self.population = 250.0;
-            }
-            _ => {}
+            element: CellElement::Air,
         }
     }
 }
@@ -143,7 +116,6 @@ struct World {
     ground: [i32; WIDTH],
 
     mass: [[f32; WIDTH]; HEIGHT],
-    new_mass: [[f32; WIDTH]; HEIGHT],
     blocks: [[Cell; WIDTH]; HEIGHT],
 }
 
@@ -155,7 +127,6 @@ impl World {
             ground: [10; WIDTH],
 
             mass: [[0.0; WIDTH]; HEIGHT],
-            new_mass: [[0.0; WIDTH]; HEIGHT],
             blocks: [[Cell::empty(); WIDTH]; HEIGHT],
         };
 
@@ -174,13 +145,14 @@ impl World {
 
     fn tick(&mut self) {
         let mut flow = 0.0;
-        let mut remaining_mass = 0.0;
         let mut blocks = self.blocks.clone();
         let mut mass = self.mass.clone();
-        let mut new_mass = self.new_mass.clone();
+        let mut new_mass = [[0.0; WIDTH]; HEIGHT];
+        let mut current_mass = 0.0;
 
-        for x in 1..WIDTH {
-            for y in 1..HEIGHT {
+        // Calculate and apply flow for each block
+        for x in 0..WIDTH {
+            for y in 0..HEIGHT {
                 // Skip inert ground blocks
                 if blocks[x][y].element == CellElement::Ground {
                     continue;
@@ -188,32 +160,32 @@ impl World {
 
                 // Custom push-only flow
                 flow = 0.0;
-                remaining_mass = mass[x][y];
+                current_mass = mass[x][y];
 
-                if remaining_mass <= 0.0 {
+                if current_mass <= 0.0 {
                     continue;
                 }
 
                 // The block bellow this one
                 if blocks[x][y + 1].element != CellElement::Ground {
-                    flow = self.get_stable_state(remaining_mass + mass[x][y + 1]) - mass[x][y + 1];
+                    flow = self.get_stable_state(current_mass + mass[x][y + 1]) - mass[x][y + 1];
                     if flow > MIN_FLOW {
                         flow *= 0.5; // leads to smoother flow
                     }
                     flow = if flow <= 0.0 {
                         0.0
-                    } else if flow >= remaining_mass.min(MAX_SPEED) {
-                        remaining_mass.min(MAX_SPEED)
+                    } else if flow >= current_mass.min(MAX_SPEED) {
+                        current_mass.min(MAX_SPEED)
                     } else {
                         flow
                     };
 
                     new_mass[x][y] -= flow;
                     new_mass[x][y + 1] += flow;
-                    remaining_mass -= flow;
+                    current_mass -= flow;
                 }
 
-                if remaining_mass <= 0.0 {
+                if current_mass <= 0.0 {
                     continue;
                 }
 
@@ -225,18 +197,18 @@ impl World {
                     }
                     flow = if flow <= 0.0 {
                         0.0
-                    } else if flow >= remaining_mass {
-                        remaining_mass
+                    } else if flow >= current_mass {
+                        current_mass
                     } else {
                         flow
                     };
 
                     new_mass[x][y] -= flow;
                     new_mass[x - 1][y] += flow;
-                    remaining_mass -= flow;
+                    current_mass -= flow;
                 }
 
-                if remaining_mass <= 0.0 {
+                if current_mass <= 0.0 {
                     continue;
                 }
 
@@ -249,49 +221,51 @@ impl World {
 
                     flow = if flow <= 0.0 {
                         0.0
-                    } else if flow >= remaining_mass {
-                        remaining_mass
+                    } else if flow >= current_mass {
+                        current_mass
                     } else {
                         flow
                     };
 
                     new_mass[x][y] -= flow;
                     new_mass[x + 1][y] += flow;
-                    remaining_mass -= flow;
+                    current_mass -= flow;
                 }
 
-                if remaining_mass <= 0.0 {
+                if current_mass <= 0.0 {
                     continue;
                 }
 
                 // Up. Only compressed water flows upwards
                 if blocks[x][y - 1].element != CellElement::Ground {
-                    flow = remaining_mass - self.get_stable_state(remaining_mass + mass[x][y - 1]);
-                    if flow > MIN_FLOW {
+                    flow = current_mass - self.get_stable_state(current_mass + mass[x][y - 1]);
+                    if flow >= MIN_FLOW {
                         flow *= 0.5;
                     }
 
                     flow = if flow <= 0.0 {
-                        0.0
-                    } else if flow >= remaining_mass.min(MAX_SPEED) {
-                        remaining_mass.min(MAX_SPEED)
+                        1.0 // tweak this value better
+                    } else if flow >= current_mass.min(MAX_SPEED) {
+                        current_mass.min(MAX_SPEED)
                     } else {
                         flow
                     };
 
                     new_mass[x][y] -= flow;
                     new_mass[x][y - 1] += flow;
-                    remaining_mass -= flow;
+                    current_mass -= flow;
                 }
             }
         }
 
-        for x in 1..WIDTH {
-            for y in 1..HEIGHT {
+        for x in 0..WIDTH {
+            for y in 0..HEIGHT {
+                // Skip ground blocks
                 if blocks[x][y].element == CellElement::Ground {
                     continue;
                 }
 
+                // Flag/unflag water blocks
                 if mass[x][y] > MIN_MASS {
                     blocks[x][y].element = CellElement::Water;
                 } else {
@@ -300,50 +274,54 @@ impl World {
             }
         }
 
+        // remove any water that has left the map
+        for x in 0..WIDTH {
+            new_mass[x][0] = 0.0;
+            new_mass[x][HEIGHT - 1] = 0.0;
+        }
+
+        for y in 0..HEIGHT {
+            new_mass[0][y] = 0.0;
+            new_mass[WIDTH - 1][y] = 0.0;
+        }
+
         self.mass = new_mass;
         self.blocks = blocks;
     }
 
     fn spawn_water(&mut self, x: usize, y: usize) {
-        self.mass[x][y] = 10.0;
+        // self.blocks[x][y] = CellWater::new(0.0);
+
         self.blocks[x][y].element = CellElement::Water;
+        self.mass[x][y] = MAX_MASS;
     }
 
     fn spawn_ground(&mut self, x: usize, y: usize) {
         self.blocks[x][y].element = CellElement::Ground;
     }
 
-    fn render_line(&self, buff: &mut [u32], x: usize, mut y0: usize, mut y1: usize, color: Color) {
-        let j: usize = 0;
-        y0 = HEIGHT - y0;
-        y1 = HEIGHT - y1;
-
-        while y0 > y1 {
-            y0 -= 1;
-            buff[y0 as usize * WIDTH + x as usize] = color.get_hex();
-        }
-    }
-
-    fn troubleshoor_renderer(&self, buff: &mut [u32]) {
-        self.render_line(buff, 10, 0, 10, Color::Red);
-        self.render_line(buff, 10, 10, 50, Color::Blue);
-    }
-
     fn get_water_color(&self, mass: f32) -> u32 {
-        Color::Blue.get_hex()
+        println!("{}", mass);
+        if mass < -0.5 { // MAX_MASS
+            // return Color::Red.get_hex();
+            return 0x70a1ff;
+        }
+
+        0x1e90ff
+        // Color::Yellow.get_hex()
     }
 
     fn render(&self, buff: &mut [u32]) {
         let blocks = self.blocks.clone();
         let mass = self.mass.clone();
 
-        for y in 1..HEIGHT {
-            for x in 1..WIDTH {
+        for y in 0..HEIGHT {
+            for x in 0..WIDTH {
                 let current_cell = self.blocks[x][y];
                 buff[y * WIDTH + x] = match current_cell.element {
                     CellElement::Water => self.get_water_color(mass[x][y]),
                     CellElement::Air => Color::Black.get_hex(),
-                    CellElement::Ground => Color::Yellow.get_hex(),
+                    CellElement::Ground => Color::White.get_hex(),
                     _ => 0,
                 }
             }
@@ -356,11 +334,11 @@ fn main() {
 
     let mut buff = vec![0; WIDTH * HEIGHT];
     let mut window = Window::new(
-        "CA",
+        "CA Water Simulation",
         WIDTH,
         HEIGHT,
         WindowOptions {
-            scale: minifb::Scale::X4,
+            scale: minifb::Scale::X2,
             ..WindowOptions::default()
         },
     ).unwrap_or_else(|e| {
